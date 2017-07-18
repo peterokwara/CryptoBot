088d1c4a5d718b5d587e739eca309d2ea48d9630
Peter.O <ojakaapeter@gmail.com>
2017-07-18 15:59:33 +0300

    Run tick method after every 30 seconds

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ API_SECRET_KEY = b'63a7b7ae598c40689e23d07f1a7e6e80'
 def main():
     print('Starting trader bot, ticking every ' + str(TICK_INTERVAL) + ' seconds')
 
+    while True:
+        start = time.time()
+        tick()
+        end = time.time()
+
+        # Sleep the thread if needed
+        if end - start <TICK_INTERVAL:
+            time.sleep(TICK_INTERVAL - (end - start))
+
+def tick():
+    pass
+
 def format_float(f):
     return "%.8" % f
 
